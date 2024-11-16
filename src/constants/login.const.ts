f816bd39260f0fc83814d98bb90f7ef8353b63c4
gaurav-jodh <gaurav.jodh@pubmatic.com>
2024-11-17 02:36:02 +0530

    Fixed some custom script issue

---
+++ src/constants/login.const.ts
@@ -0,0 +1,10 @@
+export const CREDENTIALS = {
+  standardUser: {
+    username: 'standard_user',
+    password: 'secret_sauce'
+  },
+  problemUser: {
+    username: 'problem_user',
+    password: 'secret_sauce'
+  }
+};
